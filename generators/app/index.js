6198da941ad0ab6f764a3e04e2c531587ee8670e
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2022-11-17 13:54:19 +0100

    feat: added main generator business logic

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,10 @@
-'use strict'
+/* eslint no-useless-escape:0 */
 const Generator = require('yeoman-generator')
 const chalk = require('chalk')
 const yosay = require('yosay')
+
+const fs = require('fs')
+const path = require('path')
 
 module.exports = class extends Generator {
   prompting () {
@@ -12,11 +15,49 @@ module.exports = class extends Generator {
       )
     )
 
+    const urlRegex = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
     const prompts = [
       {
+        type: 'input',
+        name: 'title',
+        message: 'Title for your website',
+        validate: Boolean
+      },
+
+      {
+        type: 'input',
+        name: 'tagLine',
+        message: 'The tagline for your website.',
+        validate: Boolean
+      },
+      {
+        type: 'input',
+        name: 'url',
+        message: 'URL for your website',
+        validate: input => urlRegex.test(input)
+      },
+      {
+        type: 'input',
+        name: 'baseUrl',
+        message: 'Base URL for your site.',
+        default: '/'
+      },
+      {
+        type: 'input',
+        name: 'gitUrl',
+        message: 'The git url',
+        validate: input => urlRegex.test(input)
+      },
+      {
         type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
+        name: 'includeGithubActions',
+        message: 'Do you want to include github actions?',
+        default: true
+      },
+      {
+        type: 'confirm',
+        name: 'includeQuickStart',
+        message: 'Do you want to start the project after the generation (release, lint, start..)?',
         default: true
       }
     ]
@@ -24,17 +65,65 @@ module.exports = class extends Generator {
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props
+      this.props.filesToSkip = []
+      this.props.shortTitle = this.props.title.replace(/-/g, '')
+
+      if (!this.props.includeGithubActions) {
+        this.props.filesToSkip = this.props.filesToSkip.concat([
+          'github/pr_health_check.yml'
+        ])
+      }
     })
   }
 
   writing () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    )
+    const getFiles = (dirPath, filesPaths = []) => {
+      const files = fs.readdirSync(dirPath)
+
+      files.forEach(file => {
+        if (fs.statSync(`${dirPath}/${file}`).isDirectory()) {
+          filesPaths = getFiles(`${dirPath}/${file}`, filesPaths)
+        } else {
+          filesPaths.push(path.join(dirPath, file))
+        }
+      })
+
+      return filesPaths
+    }
+
+    const copyFiles = (from, to) => {
+      const templatesFolder = path.join(__dirname, 'templates', from)
+      const files = getFiles(templatesFolder).map(file => file.split(`${from}/`)[1])
+
+      files.forEach(file => {
+        if (this.props.filesToSkip.includes(`${from}/${file}`)) return
+        if (file.endsWith('.ejs')) {
+          return this.fs.copy(this.templatePath(`./${from}/${file}`), this.destinationPath(`${to}/${file.replace(/^_/, '')}`))
+        }
+
+        this.fs.copyTpl(this.templatePath(`./${from}/${file}`), this.destinationPath(`${to}/${file.replace(/^_/, '')}`), this.props)
+      })
+    }
+
+    copyFiles('root', '.')
+    copyFiles('static', 'static')
+    copyFiles('img', 'static/img')
+    copyFiles('github', '.github/workflows')
+    copyFiles('docs', 'docs')
+    copyFiles('css', 'src/css')
+    copyFiles('husky', '.husky')
   }
 
   install () {
-    this.installDependencies()
+    this.installDependencies({ npm: true, bower: false, yarn: false })
+  }
+
+  end () {
+    if (this.props.includeQuickStart) {
+      this.spawnCommandSync('npm', ['run', 'lint:fix'])
+      this.spawnCommandSync('npm', ['run', 'release', '--', '--first-release'])
+      this.spawnCommandSync('npm', ['run', 'test'])
+      this.spawnCommandSync('npm', ['run', 'start'])
+    }
   }
 }
---
+++ generators/app/templates/root/_README.md
@@ -0,0 +1,37 @@
+# <%= title %>
+
+<%= tagLine %>
+
+## Website
+
+This website is built using [Docusaurus 2](https://docusaurus.io/) and [generator-fast-documentation](https://github.com/UlisesGascon/generator-fast-documentation)
+
+### Installation
+
+```
+$ npm i
+```
+
+### Local Development
+
+```
+$ npm start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+### Build
+
+```
+$ npm run build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+### Manage releases
+
+- Check [standard-version docs](https://github.com/conventional-changelog/standard-version)
+
+### Manage git Hooks
+
+- Check [Husky docs](https://github.com/typicode/husky)
